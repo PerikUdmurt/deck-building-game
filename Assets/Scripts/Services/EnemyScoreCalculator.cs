7c0adcfaaf0d62d51a9dac61ce2487301f580b5b
perik_udmurt <perevyshin.oleg@gmail.com>
2024-08-02 17:02:25 +0500

    BugFixed

---
+++ Assets/Scripts/Services/EnemyScoreCalculator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using YGameTemplate.Services.StatisticsService;
+using static CardBuildingGame.Gameplay.Characters.Character;
+
+namespace YGameTemplate.Infrastructure.Score
+{
+    public class EnemyScoreCalculator
+    {
+        private readonly Statistics _statistics;
+        private readonly Dictionary<string, int> _scores;
+
+        public EnemyScoreCalculator(Statistics statistics)
+        {
+            _statistics = statistics;
+            _scores = new();
+        }
+
+        public Dictionary<string, int> EnemyScores { get => _scores; }
+
+        public int Calculate()
+        {
+            UpdateCriteria();
+            return _scores.Values.Sum();
+        }
+
+        private void UpdateCriteria()
+        {
+            AddCriteria(CharacterType.Enemy1, 5);
+            AddCriteria(CharacterType.Enemy2, 6);
+        }
+
+        private void AddCriteria(CharacterType characterType, int multiplier)
+        {
+            int value = _statistics.GetStatisticValue($"kill_{characterType}");
+            value *= multiplier;
+
+            if (!_scores.ContainsKey($"kill_{characterType}"))
+                _scores.Add($"kill_{characterType}", value);
+            else
+                _scores[$"kill_{characterType}"] = value;
+        }
+    }
+}
--- Assets/Scripts/Services/ScoreSystem.cs
+++ Assets/Scripts/Services/ScoreSystem.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using YGameTemplate.Services.StatisticsService;
-using static CardBuildingGame.Gameplay.Characters.Character;
 
 namespace YGameTemplate.Infrastructure.Score
 {
@@ -38,43 +36,6 @@ namespace YGameTemplate.Infrastructure.Score
         {
             _totalScore = score;
             ScoreChanged?.Invoke(score);
-        }
-    }
-
-    public class EnemyScoreCalculator
-    {
-        private readonly Statistics _statistics;
-        private readonly Dictionary<string, int> _scores;
-
-        public EnemyScoreCalculator(Statistics statistics)
-        {
-            _statistics = statistics;
-            _scores = new();
-        }
-
-        public Dictionary<string, int> EnemyScores { get => _scores; }
-
-        public int Calculate()
-        {
-            UpdateCriteria();
-            return _scores.Values.Sum();
-        }
-
-        private void UpdateCriteria()
-        {
-            AddCriteria(CharacterType.Enemy1, 5);
-            AddCriteria(CharacterType.Enemy2, 6);
-        }
-
-        private void AddCriteria(CharacterType characterType, int multiplier)
-        {
-            int value = _statistics.GetStatisticValue($"kill_{characterType}");
-            value *= multiplier;
-
-            if (!_scores.ContainsKey($"kill_{characterType}"))
-                _scores.Add($"kill_{characterType}", value);
-            else
-                _scores[$"kill_{characterType}"] = value;
         }
     }
 }
